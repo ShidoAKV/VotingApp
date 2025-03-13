5ee34af38fab22261dceefb679d000ad0bdc5dfb
abhishek <abhi3vish@gmail.com>
2025-03-13 18:00:40 +0530

    deploying

--- Backend/MongoDB/index.js
+++ Backend/MongoDB/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 // const MONGOURL="mongodb://localhost:27017/VotingApp"
-// const MONGOURL="mongodb+srv://abhi3vish:abhi123@cluster0.bn1tr.mongodb.net/"
+ const MONGOURL="mongodb+srv://abhi3vish:abhi123@cluster0.bn1tr.mongodb.net/"
 mongoose.connect(process.env.MONGOURL,{
   useNewUrlParser:true,
   useUnifiedTopology:true
